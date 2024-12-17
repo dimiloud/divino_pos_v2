60d5626c244f49408f755c2dac55038becc32ee0
dimiloud <70005617+dimiloud@users.noreply.github.com>
2024-12-17 12:27:25 +0100

    Configure main URL routing

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
+    path('', RedirectView.as_view(url='/pos/'), name='home'),
     path('pos/', include('apps.pos.urls')),
     path('inventory/', include('apps.inventory.urls')),
     path('reports/', include('apps.reports.urls')),
