1413e668657a2573f5112a83ef3b5d77ba9f164a
dimiloud <70005617+dimiloud@users.noreply.github.com>
2024-12-29 14:57:31 +0100

    Fix settings module path in manage.py

--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            'Django not installed'
+            "Couldn't import Django. Are you sure it's installed?"
         ) from exc
     execute_from_command_line(sys.argv)
 
