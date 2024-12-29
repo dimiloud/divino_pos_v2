c33074753936a27517566b69b8dae6eda51fe5f2
dimiloud <70005617+dimiloud@users.noreply.github.com>
2024-12-29 15:39:20 +0100

    Update URL configuration and base template

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.pos.urls')),  # Point d'entrée principal pour le POS
+    path('', RedirectView.as_view(url='pos/', permanent=False), name='home'),
+    path('pos/', include('apps.pos.urls')),
 ]
